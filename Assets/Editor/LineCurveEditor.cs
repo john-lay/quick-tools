8d8ed7e892fc742bb73e6b493b562531fd010c8d
John Lay <jlay@logitech.com>
2021-03-19 12:03:35 +0000

    Draw a bezier curve

--- Assets/Editor/LineCurveEditor.cs
+++ Assets/Editor/LineCurveEditor.cs
@@ -9,6 +9,7 @@ public class LineCurveEditor : Editor
     private Vector2 mousePosition = Vector2.zero;
     private float minSliderValue;
     private float maxSliderValue;
+    private Texture2D BoxBorder;
 
     public void OnEnable()
     {
@@ -83,12 +84,20 @@ public class LineCurveEditor : Editor
         var end = new Vector2(curveCanvas.x + 100,  curveCanvas.y + 100);
         // Debug.LogFormat("start {0}, end {1}", start, end);
         Handles.DrawLine(start, end);
+        
+        // draw a bezier curve
+        var startPosition = new Vector2(curveCanvas.x + 10,  curveCanvas.y + 200);
+        var endPosition = new Vector2(curveCanvas.x + 200,  curveCanvas.y + 10);
+        var startTangent = new Vector2(curveCanvas.x + 10, curveCanvas.y + 20);
+        var endTangent = new Vector2(curveCanvas.x + 100,  curveCanvas.y + 100);
+        Handles.DrawBezier(startPosition, endPosition, startTangent, endTangent, Color.blue, BoxBorder, 2);
     }
 
     void DebugRect(Rect rect)
     {
         var imgPath = "Icons/square";
-        Texture2D BoxBorder = Resources.Load<Texture2D>(imgPath);
+        // Texture2D BoxBorder = Resources.Load<Texture2D>(imgPath);
+        BoxBorder = Resources.Load<Texture2D>(imgPath);
         var borderSize = 2; // Border size in pixels
         // var style = new GUIStyle();
         // style.border = new RectOffset(borderSize, borderSize, borderSize, borderSize);
