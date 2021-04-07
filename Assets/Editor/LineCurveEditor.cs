6a8fa5d41294842f18e1e5754b00454b44a582c0
John Lay <jlay@logitech.com>
2021-04-07 12:32:09 +0100

    Use a dynamic Texture2D

--- Assets/Editor/LineCurveEditor.cs
+++ Assets/Editor/LineCurveEditor.cs
@@ -60,38 +60,23 @@ public class LineCurveEditor : Editor
     {
         if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
         {
-            // var p1 = Event.current.mousePosition;
-            // var p2 = p1 + new Vector2(10, 10);
-            // Debug.LogFormat("Left-Mouse Down {0}, {1}", p1, p2);
             mousePosition = Event.current.mousePosition;
             Repaint();
         }
 
-        if (Event.current.type == EventType.Repaint && mousePosition != Vector2.zero)
+        if (Event.current.type == EventType.Repaint)
         {
-            // if (mousePosition.y > curveCanvas.yMax) Debug.Log("mouse click beneath canvas");
-            // if (mousePosition.y < curveCanvas.yMin) Debug.Log("mouse click above canvas");
-            
             // mouse position in safe zone
             if (mouseWithinCanvas())
             {
-                // Debug.LogFormat("safe zone, {0}, {1}", mousePosition.y, curveCanvas.yMax);
+                var crossLength = 10;
                 var p1 = mousePosition;
-                var p2 =  p1 + new Vector2(10, 10);
-                var p3 = p1 + new Vector2(10, 0);
-                var p4 = p1 + new Vector2(0, 10);
+                var p2 = p1 + new Vector2(crossLength, crossLength);
+                var p3 = p1 + new Vector2(crossLength, 0);
+                var p4 = p1 + new Vector2(0, crossLength);
                 Handles.DrawLine(p1, p2);
                 Handles.DrawLine(p3, p4);
             }
-
-            // var p1 = mousePosition;
-            // var p2 =  p1 + new Vector2(10, 10);
-            // var p3 = p1 + new Vector2(10, 0);
-            // var p4 = p1 + new Vector2(0, 10);
-            // Handles.DrawLine(p1, p2);
-            // Handles.DrawLine(p3, p4);
-            // Debug.Log("repainting");
-            // mousePosition = Vector2.zero;
         }
     }
 
@@ -112,11 +97,6 @@ public class LineCurveEditor : Editor
         var rect = new Rect(curveCanvas.x, curveCanvas.y - 10, curveCanvas.width, 20);
         minSliderValue = GUI.HorizontalSlider(rect, minSliderValue, 0, 1);
         
-        // var start = new Vector2(curveCanvas.x + 10, curveCanvas.y + 20);
-        // var end = new Vector2(curveCanvas.x + 100,  curveCanvas.y + 100);
-        // // Debug.LogFormat("start {0}, end {1}", start, end);
-        // Handles.DrawLine(start, end);
-        
         // draw a bezier curve
         var minPosition = curveCanvas.width * maxSliderValue + 20;
         var startPosition = new Vector2(minPosition, curveCanvas.y + 170 + 5);
@@ -124,24 +104,15 @@ public class LineCurveEditor : Editor
         var maxPosition = curveCanvas.width * minSliderValue + 20;
         var endPosition = new Vector2(maxPosition + 3,  curveCanvas.y + 5);
         
-        // var startTangent = new Vector2(curveCanvas.x + 10, curveCanvas.y + 20);
-        // var startTangent = Vector3.zero;
-        var startTangent = mousePosition;
-        // var endTangent = new Vector2(curveCanvas.x + 100,  curveCanvas.y + 100);
-        // var endTangent = Vector3.zero;
-        var endTangent = mousePosition;
-
-        // if (mouseWithinCanvas())
-        // {
-            Handles.DrawBezier(startPosition, endPosition, startTangent, endTangent, Color.black, BoxBorder, 2);
-        // }
+        Handles.DrawBezier(startPosition, endPosition, mousePosition, mousePosition, Color.black, BoxBorder, 2);
     }
 
     void DebugRect(Rect rect)
     {
-        var imgPath = "Icons/square";
+        // var imgPath = "Icons/square";
         // Texture2D BoxBorder = Resources.Load<Texture2D>(imgPath);
-        BoxBorder = Resources.Load<Texture2D>(imgPath);
+        // BoxBorder = Resources.Load<Texture2D>(imgPath);
+        BoxBorder = new Texture2D((int)curveCanvas.width, (int)curveCanvas.height);
         var borderSize = 2; // Border size in pixels
         // var style = new GUIStyle();
         // style.border = new RectOffset(borderSize, borderSize, borderSize, borderSize);
@@ -150,5 +121,4 @@ public class LineCurveEditor : Editor
         
         GUI.Box(rect, GUIContent.none, new GUIStyle(style));
     }
-
 }
